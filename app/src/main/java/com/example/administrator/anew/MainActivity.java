77dc2746c270d4a3cfb53fa80beb64b4d856bd1d
mengchen <1055199957@qq.com>
2017-04-08 13:40:26 +0800

    仿微信的摄像拍照功能

---
+++ app/src/main/java/com/example/administrator/anew/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.administrator.anew;
+
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.administrator.anew.video.CheckPermissionsUtil;
+import com.example.administrator.anew.video.JCameraView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private JCameraView mJCameraView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+
+        CheckPermissionsUtil checkPermissionsUtil = new CheckPermissionsUtil(this);
+        checkPermissionsUtil.requestAllPermission(this);
+        mJCameraView = (JCameraView) findViewById(R.id.cameraview);
+        //设置视频保存路径（如果不设置默认为Environment.getExternalStorageDirectory().getPath()）
+
+
+        mJCameraView.setAutoFoucs(false);
+        mJCameraView.setSaveVideoPath(Environment.getExternalStorageDirectory().getPath());
+        mJCameraView.setCameraViewListener(new JCameraView.CameraViewListener() {
+
+            @Override
+            public void quit() {
+                MainActivity.this.finish();
+            }
+
+            @Override
+            public void captureSuccess(Bitmap bitmap) {
+//                FileUtil.saveBitmap(bitmap);
+//                Toast.makeText(MainActivity.this, "获取到照片Bitmap:" + bitmap.getHeight(), Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void recordSuccess(String url) {
+                Toast.makeText(MainActivity.this, "获取到视频路径:" + url, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mJCameraView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mJCameraView.onPause();
+    }
+}
---
+++ app/src/main/java/com/example/administrator/anew/video/CameraFocusListener.java
@@ -0,0 +1,9 @@
+package com.example.administrator.anew.video;
+
+/**
+ * 445263848@qq.com.
+ */
+public interface CameraFocusListener {
+    public void onFocusBegin(float x, float y);
+    public void onFocusEnd();
+}
